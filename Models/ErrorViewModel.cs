4a340050b218a860e6313c658eff71b9c17f8d61
Đặng Hoàng Quân <danghoangquan@192.168.2.45>
2025-06-14 10:50:28 +0700

    Initial commit - My first MVC app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace First_App___Welcome.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
